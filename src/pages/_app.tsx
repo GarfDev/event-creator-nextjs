d2dbe2154822b36864be51c7c8feb314a9f9c878
Garfdev <garfdev.13@gmail.com>
2023-03-13 15:03:09 +0700

    feat: init taiwindscss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import Global from "@/styles/global.css";
+
+import '@/styles/base.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Global />
+    </>
+  );
 }
